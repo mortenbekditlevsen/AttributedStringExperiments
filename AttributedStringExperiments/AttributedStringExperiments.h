c729086a62329161d75dceafe9e60ddc5c72c0ff
Morten Bek Ditlevsen <bek@termestrup.dk>
2022-09-18 13:07:20 +0200

    Initial Commit

---
+++ AttributedStringExperiments/AttributedStringExperiments.h
@@ -0,0 +1,18 @@
+//
+//  AttributedStringExperiments.h
+//  AttributedStringExperiments
+//
+//  Created by Morten Bek Ditlevsen on 18/09/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AttributedStringExperiments.
+FOUNDATION_EXPORT double AttributedStringExperimentsVersionNumber;
+
+//! Project version string for AttributedStringExperiments.
+FOUNDATION_EXPORT const unsigned char AttributedStringExperimentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AttributedStringExperiments/PublicHeader.h>
+
+
